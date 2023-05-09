0460967a0c2f5900d0b98c05d08f89822c61e873
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-05-08 21:22:25 -0300

    Projeto criado

---
+++ src/main/java/com/jlcb/dslist/AlgafoodApi1Application.java
@@ -0,0 +1,13 @@
+package com.jlcb.dslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgafoodApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgafoodApi1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jlcb/dslist/AlgafoodApi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jlcb.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafoodApi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
