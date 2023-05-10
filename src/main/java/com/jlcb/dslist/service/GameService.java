efb5c1177f59f4664198dddaa3d016a458f5e87a
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-05-09 21:28:58 -0300

    GET game by id

--- src/main/java/com/jlcb/dslist/service/GameService.java
+++ src/main/java/com/jlcb/dslist/service/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.jlcb.dslist.dto.GameDTO;
 import com.jlcb.dslist.dto.GameMinDTO;
 import com.jlcb.dslist.model.Game;
 import com.jlcb.dslist.repository.GameRepository;
@@ -15,6 +17,13 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game game = gameRepository.findById(id).get();
+		return new GameDTO(game);
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> games = gameRepository.findAll();
 		return games.stream().map(x -> new GameMinDTO(x)).toList();
